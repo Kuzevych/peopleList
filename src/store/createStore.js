9458c2f2efae4d9e4897de0259337f7390095aba
John Doe <m.kuzevych@gmail.com>
2020-04-13 15:47:26 +0300

    add composeWithDevTools

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,8 +1,9 @@
 import {applyMiddleware, createStore as createReduxStore} from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import {rootReducer} from "../reducers";
 import {initialState} from "./initialState";
 import thunk from "redux-thunk";
 import {logger} from "redux-logger/src";
 
 
-export const createStore = () => createReduxStore(rootReducer,initialState,applyMiddleware(thunk, logger));
+export const createStore = () => createReduxStore(rootReducer,initialState, composeWithDevTools(applyMiddleware(thunk, logger)));
